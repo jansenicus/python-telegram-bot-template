636755b557f33575a573c5b91b0fe1c5809aecfb
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-01-25 18:58:16 +0700

    Create hello.py
    added hello

---
+++ bot/handlers/hello.py
@@ -0,0 +1,7 @@
+def hello(update, context):
+    """
+    /hello
+    just say hello and reply
+    """
+    update.message.reply_text(
+        'Hi {}, how are you?'.format(update.message.from_user.first_name))
