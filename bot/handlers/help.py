40c09a99ad5ee756649cb9a204850b6db1869c2c
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-01-25 18:59:01 +0700

    Update help.py
    added reply text

--- bot/handlers/help.py
+++ bot/handlers/help.py
@@ -1,8 +1,15 @@
-def help_command():
+def help_command(update, context):
     """
     /help:
     return this help
     """
-    print('HELP:')
+    from .index import index
+    text = '<b>HELP:</b>\n'
     for k, v in index().items():
-        print(v.__doc__)
+        #print(v.__doc__)
+        try:
+            text += v.__doc__
+        except:
+            pass
+
+    update.message.reply_text(text, parse_mode='HTML')
